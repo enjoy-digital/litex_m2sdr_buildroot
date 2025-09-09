9fee20c2c527957b482a64f41edfa485acc4db3c
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2025-09-09 11:40:54 +0200

    package/litex_m2sdr: bump version. updated litex_m2sdr_user dependencies

--- package/litex_m2sdr/litex_m2sdr.mk
+++ package/litex_m2sdr/litex_m2sdr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LITEX_M2SDR_VERSION = 8c61457a56015c16ab27e8c4bfd00a979c63342c
+LITEX_M2SDR_VERSION = 8c32d5d85653023d0e5262ed909d82564412731f
 LITEX_M2SDR_SITE = $(call github,enjoy-digital,litex_m2sdr,$(LITEX_M2SDR_VERSION))
 
 LITEX_M2SDR_LICENSE = BSD-2-Clause
